eaff5d419260a83bce766d94058c7f4be6c687d9
RTAcps <rodrigoandrade20@yahoo.com.br>
2022-03-11 18:12:56 -0300

    Inicia os testes

--- src/app/paginas/soma/soma.component.spec.ts
+++ src/app/paginas/soma/soma.component.spec.ts
@@ -2,15 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SomaComponent } from './soma.component';
 
-describe('SomaComponent', () => {
+fdescribe('SomaComponent', () => {
   let component: SomaComponent;
   let fixture: ComponentFixture<SomaComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SomaComponent ]
-    })
-    .compileComponents();
+      declarations: [SomaComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +20,40 @@ describe('SomaComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#soma', () => {
+    it('should add first and second', () => {
+      //Given
+      component.first = 1;
+      component.second = 2;
+      //When
+      component.soma();
+      //Then
+      expect(component.total).toEqual(3);
+    });
+  });
+
+  describe('#limpar', () => {
+    it('should undefined first, second and total', () => {
+      //Given
+      component.first = undefined;
+      component.second = undefined;
+      //When
+      component.limpar();
+      //Then
+      expect(component.total).toEqual(undefined);
+    });
+  });
+
+  describe('#mudar', () => {
+    it('should change mudar', () => {
+      //Given
+      component.isActive = false;
+      //When
+      component.mudar();
+      //Then
+      expect(component.total).toBeTrue;
+    });
   });
 });
